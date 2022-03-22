b2c29ed47cb4b009436cf713c23f69cea0465c06
SİBEL <gonulacarsibel@gmail.com>
2022-03-23 02:12:30 +0300

    ornek10

---
+++ conditionals/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int sayi=10;
+        if(sayi>20){
+            System.out.println("sayi 20 den büyüktür");
+
+        }else{
+            System.out.println("sayi  "+sayi+"'e eşittir");
+        }
+        }
+
+    }
+
