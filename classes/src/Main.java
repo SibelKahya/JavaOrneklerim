f3cd6a8434c1280acd1be24c1359985163a1c286
SİBEL <gonulacarsibel@gmail.com>
2022-03-23 02:11:08 +0300

    ornek8

---
+++ classes/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+   customerManager customerManager1=new customerManager();
+   customerManager1.Add();
+    }
+
+
+}
---
+++ classes/src/customerManager.java
@@ -0,0 +1,4 @@
+public class customerManager {
+    public void Add(){
+        System.out.println("müşteri eklendi..");
+    }}
