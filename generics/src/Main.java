921e34edc325eaf3a0aaec8afbb0b5c8d64b201b
SİBEL <gonulacarsibel@gmail.com>
2022-03-23 02:15:05 +0300

    ornek13

---
+++ generics/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*ArrayList<String> sehirler=new ArrayList<String>();
+        sehirler.add("Ankara");
+        sehirler.add("İstanbul");
+        sehirler.add("İzmir");
+        */
+        MyLİst<Customer> sehirler= new MyLİst<Customer>();
+        sehirler.add(new Customer());
+
+
+
+    }
+}
---
+++ generics/src/MyLİst.java
@@ -0,0 +1,8 @@
+public class MyLİst<T> {
+    public void add(T value){
+
+    }
+    public void remove(T value){
+
+    }
+}
