519403d4d89bb64dbbe6609df59365d3fae96d6f
SİBEL <gonulacarsibel@gmail.com>
2022-03-23 02:17:03 +0300

    ornek15

---
+++ inheritenceDemo/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        OgretmenKrediManager ogretmenKrediManager=new OgretmenKrediManager();
+        KrediUI krediUI=new KrediUI();
+        krediUI.KrediHesapla(new OgretmenKrediManager());
+
+
+    }
+}
