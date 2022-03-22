9d4778bceb5300ab391a5299e82c561b05bed091
SİBEL <gonulacarsibel@gmail.com>
2022-03-23 02:23:05 +0300

    ornek27

---
+++ packagesDemo/src/Main.java
@@ -0,0 +1,13 @@
+//built-in
+import matematik.DortIslem;
+import matematik.Logaritma;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        DortIslem dortIslem=new DortIslem();
+        dortIslem.topla(3,6);
+        Logaritma logaritma=new Logaritma();
+    }
+}
