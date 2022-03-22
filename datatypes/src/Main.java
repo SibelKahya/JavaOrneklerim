91e718b2c8840c43d2c8d4f02a1d0af00c841dd1
SİBEL <gonulacarsibel@gmail.com>
2022-03-23 02:12:57 +0300

    ornek11

---
+++ datatypes/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int sayi=12;
+        int sayi2=120;
+        byte sayi3=55;
+        char karakter='A';
+        boolean dogruMu=true;
+    }
+}
